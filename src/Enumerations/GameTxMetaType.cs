c7a6e518f8978ed75297f6db285ecb6c2bb592e1
Perkins Jon Ong <official.pjo@outlook.com>
2020-10-19 11:33:08 +0800

    added enumerations

---
+++ src/Enumerations/GameTxMetaType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADALottoModels.Enumerations
+{
+    public enum GameTxMetaType
+    {
+        Start,
+        End,
+        TicketPurchase
+    }
+}
