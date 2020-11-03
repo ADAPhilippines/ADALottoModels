344931ae283c5fea6f67e7a6f1952598276aaa7f
Perkins Jon Ong <official.pjo@outlook.com>
2020-11-04 02:46:15 +0800

    Changes to models

--- src/ADALottoGameWinner.cs
+++ src/ADALottoGameWinner.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using ADALotto.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace ADALottoModels
 {
     public class ADALottoGameWinner
     {
         public string Address { get; set; } = string.Empty;
+        public float DrawBlockId { get; set; }
         public DateTime DrawDate { get; set; }
         public double Prize { get; set; }
-        public bool IsRewarded { get; set; } = false;
+        public Transaction? RewardTx { get; set; }
     }
 }
