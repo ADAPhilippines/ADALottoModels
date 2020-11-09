a9a647733dafcb97f03a3a916f9facf35194aef2
Perkins Jon Ong <official.pjo@outlook.com>
2020-11-10 05:03:37 +0800

    modifications to ALGameState

--- src/ALGameState.cs
+++ src/ALGameState.cs
@@ -7,16 +7,15 @@ namespace ADALottoModels
 {
     public class ALGameState
     {
-        public string HostWallet { get; set; } = string.Empty;
-        public long StartBlock { get; set; } = 0;
-        public long CurrentPot { get; set; } = 0;
-        public IEnumerable<ALResult> PreviousResults { get; set; } = new List<ALResult>();
-        public IEnumerable<ALWinner> PreviousWinners { get; set; } = new List<ALWinner>();
+        public Block? StartBlock { get; set; }
+        public long? CurrentPot { get; set; }
+        public IEnumerable<ALResult>? PreviousResults { get; set; }
+        public IEnumerable<ALWinner>? PreviousWinners { get; set; }
         public Transaction? GameGenesisTx { get; set; }
         public ALGameGenesisTxMeta? GameGenesisTxMeta { get; set; }
-        public long NextDrawBlock { get; set; } = 0;
-        public long PrevDrawBlock { get; set; } = 0;
-        public bool IsDrawing { get; set; } = false;
-        public bool IsRunning { get; set; } = false;
+        public Block? NextDrawBlock { get; set; }
+        public Block? PrevDrawBlock { get; set; }
+        public bool? IsDrawing { get; set; }
+        public bool? IsRunning { get; set; }
     }
 }
