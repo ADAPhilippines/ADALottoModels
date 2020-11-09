e46ae459e804e0ff02568b3f7f611ce2d06b82fa
Perkins Jon Ong <official.pjo@outlook.com>
2020-11-10 04:39:41 +0800

    added blockNo property block

--- src/Block.cs
+++ src/Block.cs
@@ -7,6 +7,7 @@ namespace ADALotto.Models
     public class Block
     {
         public long Id { get; set; }
+        public long BlockNo { get; set; }
         public long EpochNo { get; set; }
         public long Size { get; set; }
         public DateTime Time { get; set; }
