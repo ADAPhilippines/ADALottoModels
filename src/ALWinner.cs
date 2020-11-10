b7eab4beb5c2d0d6354beda49e0bef809d105e6e
Perkins Jon Ong <official.pjo@outlook.com>
2020-11-10 18:36:43 +0800

    removed drawDate from ALWinner

--- src/ALWinner.cs
+++ src/ALWinner.cs
@@ -8,7 +8,6 @@ namespace ADALottoModels
     {
         public string Address { get; set; } = string.Empty;
         public Block DrawBlock { get; set; } = new Block();
-        public DateTime DrawDate { get; set; }
         public long Prize { get; set; }
         public Transaction? RewardTx { get; set; }
     }
