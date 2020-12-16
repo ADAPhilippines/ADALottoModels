e025da873e4f3d41d8d4f14910d81a113b28434a
Clark Alesna <clark_alesna@hotmail.com>
2020-12-16 11:59:57 +0800

    Update to new DB-SYNC for Allegra

--- src/Transaction.cs
+++ src/Transaction.cs
@@ -8,8 +8,8 @@ namespace ADALotto.Models
     public class Transaction
     {
         public long? Id { get; set; }
-        public long? Block { get; set; }
-        public Block Block1 { get; set; } = new Block();
+        public long? BlockId { get; set; }
+        public Block Block { get; set; } = new Block();
         public IEnumerable<TransactionMeta>? TxMetadata { get; set; }
         public IEnumerable<TransactionIn>? InTxIns { get; set; }
         public int[]? Hash { get; set; }
